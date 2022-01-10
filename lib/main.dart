ad677be150fd463f7ce58afa269664d30b1fcd62
4xMafole <4cruxt.unknown@gmail.com>
2022-01-10 22:38:11 +0300

    Done asset design page

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:kibasi/auth/forgot_password_page.dart';
+import 'package:kibasi/content/assets/edit_bus_page.dart';
+import 'package:kibasi/content/assets/edit_driver.dart';
 import 'package:kibasi/content/dashboard/dashboard_page.dart';
-import 'package:kibasi/content/profile/asset_page.dart';
+import 'package:kibasi/content/assets/asset_page.dart';
 import 'package:kibasi/content/profile/profile_page.dart';
 import 'package:kibasi/onboard/onboard_page.dart';
 
