e737a72414650acde26cd5bf29f898157e2218b2
4xMafole <4cruxt.unknown@gmail.com>
2022-01-31 19:38:22 +0300

    Added auth validators.

---
+++ lib/utils/validator.dart
@@ -0,0 +1,32 @@
+class Validator {
+  static String? validateName({required String name}) {
+    if (name.isEmpty) {
+      return 'Please enter username';
+    }
+
+    return null;
+  }
+
+  static String? validateEmail({required String email}) {
+    RegExp emailExp = RegExp(
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+
+    if (email.isEmpty) {
+      return 'Please enter email';
+    } else if (!emailExp.hasMatch(email)) {
+      return 'Please enter a correct email';
+    }
+
+    return null;
+  }
+
+  static String? validatePassword({required String password}) {
+    if (password.isEmpty) {
+      return 'Please enter password';
+    } else if (password.length < 8) {
+      return 'Enter a password with length atleast 6 characters';
+    }
+
+    return null;
+  }
+}
