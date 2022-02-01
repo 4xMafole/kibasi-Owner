12d638806d1c6c094696090a2b78d8ba0a0cf255
4xMafole <4cruxt.unknown@gmail.com>
2022-02-01 22:09:41 +0300

    Added firebase registration

--- lib/auth/register_page.dart
+++ lib/auth/register_page.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kibasi/auth/login_page.dart';
+import 'package:kibasi/content/profile/profile_page.dart';
+import 'package:kibasi/utils/firebase/fire_auth.dart';
+import 'package:kibasi/utils/validator.dart';
 import 'package:kibasi/widget/bezier.dart';
 import 'package:kibasi/utils/custom_color.dart' as color;
 
@@ -12,51 +17,85 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _registerFormKey = GlobalKey<FormState>();
+
+  final _nameNode = FocusNode();
+  final _emailNode = FocusNode();
+  final _passwordNode = FocusNode();
+  final _confirmPasswordNode = FocusNode();
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _conPasswordController = TextEditingController();
+
+  bool _isProcessing = false;
 
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: Container(
-        height: height,
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              top: -MediaQuery.of(context).size.height * .15,
-              right: -MediaQuery.of(context).size.width * .4,
-              child: CustomBezier(),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(height: height * .2),
-                    _title(),
-                    SizedBox(
-                      height: 50,
+    return GestureDetector(
+      onTap: () {
+        _nameNode.unfocus();
+        _emailNode.unfocus();
+        _passwordNode.unfocus();
+        _confirmPasswordNode.unfocus();
+      },
+      child: Scaffold(
+        body: Container(
+          height: height,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: -MediaQuery.of(context).size.height * .15,
+                right: -MediaQuery.of(context).size.width * .4,
+                child: CustomBezier(),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  child: Form(
+                    key: _registerFormKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(height: height * .2),
+                        _title(),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        _emailPasswordWidget(),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        _isProcessing
+                            ? CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                  color.AppColor.blue,
+                                ),
+                              )
+                            : _registerButton(),
+                        SizedBox(height: height * 0.05),
+                        _loginAccountLabel(),
+                      ],
                     ),
-                    _emailPasswordWidget(),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    _submitButton(),
-                    SizedBox(height: height * 0.05),
-                    _loginAccountLabel(),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
-
-  Widget _entryField(String title, {bool isPassword = false}) {
+  Widget _entryField(
+    String title,
+    TextEditingController textController,
+    FocusNode focusField, {
+    bool isPassword = false,
+  }) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10),
       child: Column(
@@ -69,69 +108,114 @@ class _RegisterPageState extends State<RegisterPage> {
           SizedBox(
             height: 10,
           ),
-          TextField(
-              obscureText: isPassword,
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  fillColor: Color(0xfff3f3f4),
-                  filled: true))
+          TextFormField(
+            controller: textController,
+            focusNode: focusField,
+            obscureText: isPassword,
+            validator: (value) => isPassword
+                ? Validator.validatePassword(
+                    password: _passwordController.text,
+                    confirmPassword: _conPasswordController.text,
+                  ) //confirm password should be checked
+                : textController == _emailController
+                    ? Validator.validateEmail(email: value!)
+                    : Validator.validateName(name: value!),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              fillColor: color.AppColor.inputColor,
+              filled: true,
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Widget _submitButton() {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.symmetric(vertical: 15),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-              color: color.AppColor.paleBlue.withOpacity(0.6),
-              offset: Offset(2, 4),
-              blurRadius: 20,
-              spreadRadius: 5,)
-          ],
-          color: color.AppColor.blue),
-      child: Text(
-        'Register',
-        style: TextStyle(fontSize: 20, color: Colors.white),
+  Widget _registerButton() {
+    return InkWell(
+      onTap: () async {
+        _nameNode.unfocus();
+        _emailNode.unfocus();
+        _passwordNode.unfocus();
+        _confirmPasswordNode.unfocus();
+
+        if (_registerFormKey.currentState!.validate()) {
+          setState(() {
+            _isProcessing = true;
+          });
+
+          User? user = await FireAuth.registerUsingEmailPassword(
+            name: _nameController.text,
+            email: _emailController.text,
+            password: _passwordController.text,
+          );
+
+          setState(() {
+            _isProcessing = false;
+          });
+
+          if (user != null) {
+            Get.offAll(ProfilePage(
+              user: user,
+            ));
+          }
+        }
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 15),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: color.AppColor.paleBlue.withOpacity(0.6),
+                offset: Offset(2, 4),
+                blurRadius: 20,
+                spreadRadius: 5,
+              )
+            ],
+            color: color.AppColor.blue),
+        child: Text(
+          'Register',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
       ),
     );
   }
 
   Widget _loginAccountLabel() {
     return Container(
-        margin: EdgeInsets.symmetric(vertical: 20),
-        padding: EdgeInsets.all(15),
-        alignment: Alignment.bottomCenter,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Already have an account ?',
-              style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
+      margin: EdgeInsets.symmetric(vertical: 20),
+      padding: EdgeInsets.all(15),
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Already have an account ?',
+            style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                  (route) => false);
+            },
+            child: Text(
+              'Login',
+              style: TextStyle(
+                  color: color.AppColor.paleBlue,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600),
             ),
-            SizedBox(
-              width: 10,
-            ),
-            InkWell(
-              onTap: () {
-                Navigator.pushAndRemoveUntil(
-                    context, MaterialPageRoute(builder: (context) => LoginPage()), (route) => false);
-              },
-              child: Text(
-                'Login',
-                style: TextStyle(
-                    color: color.AppColor.paleBlue,
-                    fontSize: 13,
-                    fontWeight: FontWeight.w600),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -153,12 +237,29 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget _emailPasswordWidget() {
     return Column(
       children: <Widget>[
-        _entryField("Username"),
-        _entryField("Email"),
-        _entryField("Password", isPassword: true),
-        _entryField("Confirm Password", isPassword: true),
+        _entryField(
+          "Username",
+          _nameController,
+          _nameNode,
+        ),
+        _entryField(
+          "Email",
+          _emailController,
+          _emailNode,
+        ),
+        _entryField(
+          "Password",
+          _passwordController,
+          _passwordNode,
+          isPassword: true,
+        ),
+        _entryField(
+          "Confirm Password",
+          _conPasswordController,
+          _confirmPasswordNode,
+          isPassword: true,
+        ),
       ],
     );
   }
-
 }
--- lib/utils/firebase/firedb.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-
-class Firedb {
-  //Initialize firebase
-  Future<FirebaseApp> _initializeFirebase() async {
-    FirebaseApp firebaseApp = await Firebase.initializeApp();
-    return firebaseApp;
-  }
-}
