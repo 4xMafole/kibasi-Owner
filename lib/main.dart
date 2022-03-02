8aa9ac68574809af0f035c4bc02b47391b4b9313
4xMafole <4cruxt.unknown@gmail.com>
2022-03-02 12:04:22 +0300

    Added views to the main page

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,9 @@ import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'package:kibasi/auth/forgot_password_page.dart';
-import 'package:kibasi/content/assets/asset_page.dart';
-import 'package:kibasi/content/assets/edit_bus_page.dart';
-import 'package:kibasi/content/assets/edit_driver_page.dart';
+import 'package:kibasi/content/assets/views/asset_page.dart';
+import 'package:kibasi/content/assets/views/edit_bus_page.dart';
+import 'package:kibasi/content/assets/views/edit_driver_page.dart';
 import 'package:kibasi/content/dashboard/dashboard_page.dart';
 import 'package:kibasi/content/profile/profile_page.dart';
 import 'package:kibasi/firebase_options.dart';
